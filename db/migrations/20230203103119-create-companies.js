3ea79645dc598d6b9c5528f660242d88293ef061
Aryan Goel <aryangoel6900@yahoo.in>
2023-02-03 16:02:27 +0530

    chore: created models and set up db

---
+++ db/migrations/20230203103119-create-companies.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Companies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      companyId: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      ceo: {
+        type: Sequelize.STRING
+      },
+      tags: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Companies');
+  }
+};
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const router = require('./routes/routes'); 
+const router = require('./src/routes/routes');
 
 app.route('/', router);
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
